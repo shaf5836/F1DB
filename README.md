9cf076603944dfb62ceb74070e17c39164c35fca
shaf5836 <shafkathossain36@gmail.com>
2026-04-26 02:57:55 +0600

    Updating package files, readme and db.js file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # F1DB
-This is a Database focused project on F1 which showcases huge number of f1 data.
+This is a Database focused project on F1 which showcases F1 data with the help of various features.
---
+++ config/db.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+const envLoaded = dotenv.config(); // loading values from .env file
+if(envLoaded.error){
+    throw new Error(".env file is missing or failed to load");
+}
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'f1_db'
+});
+
+// Error management 
+db.connect((err) => {
+    if(err){
+        console.error("DB connection failed:", err);
+    }
+    else{
+        console.log(`Connected to ${process.env.DB_NAME}`);
+    }
+});
+
+export default db;
